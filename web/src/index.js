d5566bd0d0d7a31bfd3ae356da2378c5a8cd06cc
Camilo Reyes <reyesc@comcast.net>
2020-12-12 06:36:56 -0600

    Add missing scaffold styles on the page

--- web/src/index.js
+++ web/src/index.js
@@ -5,6 +5,7 @@ import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
 import './index.css'
+import './scaffold.css'
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
